449a70fcd392f62245a5981ea5cb52175b8f0018
GuillermoBPY <guillermo-benitez@outlook.com>
2023-01-03 15:53:55 -0300

    Actualizado Navbar

--- src/js/main.js
+++ src/js/main.js
@@ -4,8 +4,6 @@ const menu = document.querySelector(".porfolio__home--navbar--menu");
 menuIcon.addEventListener("click", function () {
   menu.classList.toggle("menushow");
   menu.classList.value.includes("menushow")
-    ? (document.getElementById("hambmenuicon").src =
-        "./src/img/icons/closemenu.svg")
-    : (document.getElementById("hambmenuicon").src =
-        "./src/img/icons/openmenu.svg");
+    ? (document.getElementById("hambmenuicon").src="./src/img/icons/closemenu.svg")
+    : (document.getElementById("hambmenuicon").src="./src/img/icons/openmenu.svg");
 });
