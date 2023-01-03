faa0303eb2f02fddd650f7f3ff560e58bac88b83
GuillermoBPY <guillermo-benitez@outlook.com>
2023-01-02 23:32:50 -0300

    First Commit

---
+++ src/js/main.js
@@ -0,0 +1,11 @@
+const menuIcon = document.getElementById("hambmenuicon");
+const menu = document.querySelector(".porfolio__home--navbar--menu");
+
+menuIcon.addEventListener("click", function () {
+  menu.classList.toggle("menushow");
+  menu.classList.value.includes("menushow")
+    ? (document.getElementById("hambmenuicon").src =
+        "./src/img/icons/closemenu.svg")
+    : (document.getElementById("hambmenuicon").src =
+        "./src/img/icons/openmenu.svg");
+});
