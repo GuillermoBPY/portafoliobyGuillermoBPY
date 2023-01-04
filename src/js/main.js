95aa384a3efc5a56a5045a05af96b9ed51dc9d10
GuillermoBPY <guillermo-benitez@outlook.com>
2023-01-03 21:19:45 -0300

    Actualizado Menu

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,8 @@
 const menuIcon = document.getElementById("hambmenuicon");
 const menu = document.querySelector(".porfolio__home--navbar--menu");
+const menuList = document.querySelectorAll(".porfolio__home--navbar--menu li");
+
+
 
 menuIcon.addEventListener("click", function () {
   menu.classList.toggle("menushow");
@@ -7,3 +10,7 @@ menuIcon.addEventListener("click", function () {
     ? (document.getElementById("hambmenuicon").src="./src/img/icons/closemenu.svg")
     : (document.getElementById("hambmenuicon").src="./src/img/icons/openmenu.svg");
 });
+
+menuList.forEach(e => { e.addEventListener("click",function(){
+  menu.classList.remove("menushow");
+})})
