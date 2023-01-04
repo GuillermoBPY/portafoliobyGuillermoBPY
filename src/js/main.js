316d610c5da6166b99194543640ffac31e5dc295
GuillermoBPY <guillermo-benitez@outlook.com>
2023-01-03 22:17:32 -0300

    Actualizado

--- src/js/main.js
+++ src/js/main.js
@@ -2,24 +2,20 @@ const menuIcon = document.getElementById("hambmenuicon");
 const menu = document.querySelector(".porfolio__home--navbar--menu");
 const menuList = document.querySelectorAll(".porfolio__home--navbar--menu li");
 
-
-
 menuIcon.addEventListener("click", function () {
   menu.classList.toggle("menushow");
-  changeMenu()
+  changeMenu();
 });
 
-menuList.forEach(e => { e.addEventListener("click",function(){
-  menu.classList.remove("menushow");
-  changeMenu()
-})})
+menuList.forEach((e) => {
+  e.addEventListener("click", function () {
+    menu.classList.remove("menushow");
+    changeMenu();
+  });
+});
 
-
-
-
-
-function changeMenu (){
+function changeMenu() {
   menu.classList.value.includes("menushow")
-    ? (document.getElementById("hambmenuicon").src="./src/img/icons/closemenu.svg")
-    : (document.getElementById("hambmenuicon").src="./src/img/icons/openmenu.svg");
+    ? (menuIcon.src = "./src/img/icons/closemenu.svg")
+    : (menuIcon.src = "./src/img/icons/openmenu.svg");
 }
